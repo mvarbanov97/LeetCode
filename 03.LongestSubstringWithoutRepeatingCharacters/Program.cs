efb273f6670d352fb2068eb667ac3578fc2964b7
Momchil Varbanov <momchil.varbanov97@gmail.com>
2021-07-14 23:59:48 +0300

    Problem  03 and 04 Solved
    03.LongestSubstringWithoutRepeatingCharacters and MedianOfTwoSortedArrays solved

---
+++ 03.LongestSubstringWithoutRepeatingCharacters/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03.LongestSubstringWithoutRepeatingCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testString = "abcabcbb"; // 3
+            var testString2 = "bbbbbb"; // 1
+            var testString3 = "pwwkew"; // 3
+            var testString4 = ""; // 0
+            var testString5 = "au"; // 2
+            var testString6 = "dvdf"; //3
+
+
+            var result = LengthOfLongestSubstring(testString);
+            var result2 = LengthOfLongestSubstring(testString2);
+            var result3 = LengthOfLongestSubstring(testString3);
+            var result4 = LengthOfLongestSubstring(testString4);
+            var result5 = LengthOfLongestSubstring(testString5);
+            var result6 = LengthOfLongestSubstring(testString6);
+            Console.WriteLine(result);
+            Console.WriteLine(result2);
+            Console.WriteLine(result3);
+            Console.WriteLine(result4);
+            Console.WriteLine(result5);
+            Console.WriteLine(result6);
+        }
+
+        public static int LengthOfLongestSubstring(string str)
+        {
+            Dictionary<char, int> seen = new Dictionary<char, int>();
+            int maximum_length = 0;
+
+            // starting the inital point of window to index 0
+            int start = 0;
+
+            for (int index = 0; index < str.Length; index++)
+            {
+                // Checking if we have already seen the element or not
+                if (seen.ContainsKey(str[index]))
+                {
+                    // If we have seen the number, move the start pointer
+                    // to position after the last occurrence
+                    start = Math.Max(start, seen[str[index]] + 1);
+                }
+
+                // Updating the last seen value of the character
+                seen[str[index]] = index;
+                maximum_length = Math.Max(maximum_length, index - start + 1);
+            }
+            return maximum_length;
+        }
+    }
+}
---
+++ 04.MedianOfTwoSortedArrays/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.MedianOfTwoSortedArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // [1,3]
+            // [2]
+            // 
+            // [1,2]
+            // [3,4]
+            // 
+            // [0,0]
+            // [0,0]
+            // 
+            // []
+            // [1]
+            // 
+            // [2]
+            // []
+            //
+            // []
+            // [1,2,3,4,5]
+
+            int[] firstNums = { };
+            int[] secondNums = { 1, 2, 3, 4, 5 };
+
+            var median = FindMedianSortedArrays(firstNums, secondNums);
+            Console.WriteLine(median);
+        }
+
+        public static double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            int[] combined = new int[nums1.Length + nums2.Length];
+            if (nums1.Length == 0)
+            {
+                combined = nums2.Concat(nums1).OrderBy(x => x).ToArray();
+            }
+            else
+            {
+                combined = nums1.Concat(nums2).OrderBy(x => x).ToArray();
+            }
+
+            double result = 0;
+
+
+            if (combined.Length % 2 == 0)
+            {
+                var firstIndex = combined.Length / 2 - 1;
+                var secondIndex = combined.Length / 2;
+                result = (double)(combined[firstIndex] + combined[secondIndex]) / 2;
+            }
+            else if (combined.Length % 3 > 0 || combined.Length == 3 || combined.Length % 3 == 0)
+            {
+                var index = Math.Floor((decimal)combined.Length / 2);
+                result = combined[(int)index];
+            }
+            else
+            {
+                result = combined.First();
+            }
+
+            return result;
+        }
+    }
+}
