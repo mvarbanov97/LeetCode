1e7ef99efa7dceac325d7ef4d82f86e7101d132c
Momchil Varbanov <momchil.varbanov97@gmail.com>
2021-07-15 00:02:08 +0300

    Problem 05 and 06 solved
    05.LongestPalindromicSubstring and 06.ZigZagConversion solved

---
+++ 05.LongestPalindromicSubstring/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Text;
+
+namespace _05.LongestPalindromicSubstring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test1 = "babad"; // bab
+            var test2 = "ac"; // a
+            var test3 = "a"; // a
+            var test4 = "bb"; // bb
+            var test5 = "ccc"; // ccc
+            var test6 = "abcdeedcba"; // abcdeedcba
+
+            var result1 = LongestPalindrome(test1);
+            Console.WriteLine(result1);
+
+            var result1Optimized = LongestPalindromeOptimized(test1);
+            Console.WriteLine(result1Optimized);
+
+            var result2 = LongestPalindrome(test2);
+            Console.WriteLine(result2);
+
+            var result3 = LongestPalindrome(test3);
+            Console.WriteLine(result3);
+
+            var result4 = LongestPalindrome(test4);
+            Console.WriteLine(result4);
+
+            var result5 = LongestPalindrome(test5);
+            Console.WriteLine(result5);
+
+            var result6 = LongestPalindrome(test6);
+            Console.WriteLine(result6);
+
+            var timeLimitTest = "kztakrekvefgchersuoiuatzlmwynzjhdqqftjcqmntoyckqfawikkdrnfgbwtdpbkymvwoumurjdzygyzsbmwzpcxcdmmpwzmeibligwiiqbecxwyxigikoewwrczkanwwqukszsbjukzumzladrvjefpegyicsgctdvldetuegxwihdtitqrdmygdrsweahfrepdcudvyvrggbkthztxwicyzazjyeztytwiyybqdsczozvtegodacdokczfmwqfmyuixbeeqluqcqwxpyrkpfcdosttzooykpvdykfxulttvvwnzftndvhsvpgrgdzsvfxdtzztdiswgwxzvbpsjlizlfrlgvlnwbjwbujafjaedivvgnbgwcdbzbdbprqrflfhahsvlcekeyqueyxjfetkxpapbeejoxwxlgepmxzowldsmqllpzeymakcshfzkvyykwljeltutdmrhxcbzizihzinywggzjctzasvefcxmhnusdvlderconvaisaetcdldeveeemhugipfzbhrwidcjpfrumshbdofchpgcsbkvaexfmenpsuodatxjavoszcitjewflejjmsuvyuyrkumednsfkbgvbqxfphfqeqozcnabmtedffvzwbgbzbfydiyaevoqtfmzxaujdydtjftapkpdhnbmrylcibzuqqynvnsihmyxdcrfftkuoymzoxpnashaderlosnkxbhamkkxfhwjsyehkmblhppbyspmcwuoguptliashefdklokjpggfiixozsrlwmeksmzdcvipgkwxwynzsvxnqtchgwwadqybkguscfyrbyxudzrxacoplmcqcsmkraimfwbauvytkxdnglwfuvehpxd";
+            var timeLimitResult = LongestPalindrome(timeLimitTest);
+            Console.WriteLine(timeLimitResult);
+        }
+
+        public static string LongestPalindrome(string str)
+        {
+            var result = string.Empty;
+
+            if (str.Length == 1 || (str.Length == 2 && str[0] != str[1]))
+                return result = str[0].ToString();
+
+            // All subStrings of length 1 are palindromes
+            int maxLength = 1, start = 0;
+
+            // Nested loop to mark start and end index
+            for (int i = 0; i < str.Length; i++)
+            {
+                for (int j = i; j < str.Length; j++)
+                {
+                    int flag = 1;
+
+                    // Check palindrome
+                    for (int k = 0; k < (j - i + 1) / 2; k++)
+                    {
+                        if (str[i + k] != str[j - k])
+                        {
+                            flag = 0;
+                            break;
+                        }
+                    }
+
+                    if (flag != 0 && (j-i + 1) > maxLength)
+                    {
+                        start = i;
+                        maxLength = j - i + 1;
+                    }
+                }
+            }
+
+            for (int l = start; l <= start + maxLength - 1; l++)
+            {
+                result += str[l];
+            }
+            
+            return result;
+        }
+
+        public static string LongestPalindromeOptimized(string s)
+        {
+            int maxLength = 0, startIndex = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                int start = i, end = i;
+                while (end < s.Length - 1 && s[start] == s[end + 1])
+                    end++;
+
+                while (end < s.Length - 1 && start > 0 && s[start - 1] == s[end + 1])
+                {
+                    start--;
+                    end++;
+                }
+                if (maxLength < end - start + 1)
+                {
+                    maxLength = end - start + 1;
+                    startIndex = start;
+                }
+            }
+            return s.Substring(startIndex, maxLength);
+        }
+    }
+}
---
+++ 06.ZigZagConversion/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06.ZigZagConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test1String = "PAYPALISHIRING";
+            var test1Rows = 3;
+
+            var result1 = Convert(test1String, test1Rows);
+            Console.WriteLine(result1);
+        }
+
+        public static string Convert(string s, int numRows)
+        {
+            if (numRows == 1)
+                return s;
+
+            var result = new StringBuilder();
+
+            List<StringBuilder> rows = new List<StringBuilder>();
+
+            for (int i = 0; i < Math.Min(numRows, s.Length); i++)
+                rows.Add(new StringBuilder());
+
+            int currRow = 0;
+            bool goingDown = false;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                rows[currRow].Append(s[i]);
+                if (currRow == 0 || currRow == numRows - 1) 
+                    goingDown = !goingDown;
+
+                currRow += goingDown ? 1 : -1;
+            }
+
+            foreach (var row in rows)
+            {
+                result.Append(row);
+            }
+
+            return result.ToString();
+        }
+    }
+}
